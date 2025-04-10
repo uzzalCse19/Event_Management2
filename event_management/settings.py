0759dac421ba771d32d207089af2907f6e48b54b
uzzalCse19 <uzzalhasan7878@gmail.com>
2025-04-10 20:15:09 +0600

    Ready For Deployment

--- event_management/settings.py
+++ event_management/settings.py
@@ -16,8 +16,9 @@ SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS=['https://*.onrender.com','http://127.0.0.1:8000']
+CSRF_USER_MODEL='users.Customers'
 
 # Application definition
 
